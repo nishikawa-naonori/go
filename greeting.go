4a0902a6cb642c703de0b96bcce86f42349a15a1
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-04-28 14:47:33 +0900

    Initial commit

---
+++ greeting.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+
+    fmt.Println("Good morning888888")
+
+    fmt.Println("Good afrernoon")
+
+    fmt.Println("Good evening")
+
+}
+// go build greeting.go
+// ./greeting
+
+// go run greeting.go
+
